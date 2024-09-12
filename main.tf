30f196a09187529a4764b7b1657b41fb5d815b78
gregolly <gregollyff@hotmail.com>
2024-09-12 10:48:15 -0400

    yml archives to set ansible with terraform

--- main.tf
+++ main.tf
@@ -18,8 +18,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-05134c8ef96964280"
   instance_type = "t2.micro"
   key_name = "iac-alura"
+    # user_data = <<-EOF
+    #                 #!/bin/bash
+    #                 cd /home/ubuntu
+    #                 echo "<h1>Feito com Terraform</h1>" > index.html
+    #                 nohup busybox httpd -f -p 8080 &
+    #               EOF
 
   tags = {
-    Name = "Primeira instancia"
+    Name = "Teste AWS"
   }
 }
